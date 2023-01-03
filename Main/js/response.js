952bd3ca06d928258cbd999f4ea19c0be603f45e
Sanju <anutanwer12@gmail.com>
2023-01-03 22:16:57 +0530

    Update response.js

--- Main/js/response.js
+++ Main/js/response.js
@@ -11,7 +11,8 @@ function getBotResponse(input) {
     // Simple responses
     if (input == "hello") {
         return "Namasta!";
-    } else if (input == "goodbye") {
+    } 
+    else if (input == "goodbye") {
         return "Talk to you later!";
     } 
     else if (input == "Are you a BOT!") {
@@ -19,6 +20,15 @@ function getBotResponse(input) {
     }
     else if (input == "Can you play game?") {
         return "Yes, I can play rock paper scissors game!"
+    }
+    else if (input == "i need help") {
+        return "I am here only for helping you :) Go ahead ask you problem!"
+    }
+    else if (input == "how are you") {
+        return "I am fine! I hope you also safe and secure or happy with your family"
+    }
+    else if (input == "i am sad") {
+        return "OOPS :( I am also sad after hear this :| but here I have a joke for you - Did you hear about the mathematician who’s afraid of negative numbers? He’ll stop at nothing to avoid them."
     }
     else {
         return "Try asking something else!";
